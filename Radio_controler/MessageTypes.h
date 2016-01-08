38275e5914778586f6edfc2dd126e03971b39b31
Gal Hershcovich <hershco@gmail.com>
2016-01-08 14:55:58 +0200

    test branch

---
+++ Radio_controler/MessageTypes.h
@@ -0,0 +1,19 @@
+/*
+ * MessageTypes.h
+ *
+ *  Created on: Jan 7, 2016
+ *      Author: hershco
+ */
+
+#ifndef MESSAGETYPES_H_
+#define MESSAGETYPES_H_
+
+#include <ctype.h>
+
+typedef struct RadioCommand{
+	uint8_t commandType;
+	uint16_t stationNumber;
+};
+
+
+#endif /* MESSAGETYPES_H_ */
